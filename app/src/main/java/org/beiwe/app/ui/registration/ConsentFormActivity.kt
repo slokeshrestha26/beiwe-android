b2e68db482ea1c0796057b48915b3f6850446d24
Eli Jones <eli@zagaran.com>
2020-07-31 06:03:35 -0400

    [kotlinize] consentform

---
+++ app/src/main/java/org/beiwe/app/ui/registration/ConsentFormActivity.kt
@@ -0,0 +1,58 @@
+package org.beiwe.app.ui.registration
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_consent_form.*
+import org.beiwe.app.R
+import org.beiwe.app.RunningBackgroundServiceActivity
+import org.beiwe.app.networking.SurveyDownloader
+import org.beiwe.app.storage.PersistentData
+import org.beiwe.app.storage.TextFileManager
+import org.beiwe.app.ui.LoadingActivity
+
+class ConsentFormActivity : RunningBackgroundServiceActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consent_form)
+        consent_form_body.text = PersistentData.getConsentFormText()
+    }
+
+    /** On the press of the do not consent button, we pop up an alert, allowing the user
+     * to press "Cancel" if they did not mean to press the do not consent.  */
+    fun doNotConsentButton(view: View?) {
+        val alertBuilder = AlertDialog.Builder(this@ConsentFormActivity)
+        alertBuilder.setTitle(getString(R.string.doNotConsentButton))
+        alertBuilder.setMessage(getString(R.string.doNotConsentAlert))
+        alertBuilder.setPositiveButton(getString(R.string.i_understand_button_text)) { dialog, which ->
+            finish()
+            System.exit(0)
+        }
+        alertBuilder.setNegativeButton(getString(R.string.alert_cancel_button_text), DialogInterface.OnClickListener { dialog, which ->
+            return@OnClickListener
+        })
+        alertBuilder.create().show()
+    }
+
+    fun consentButton(view: View?) {
+        PersistentData.setRegistered(true)
+        PersistentData.loginOrRefreshLogin()
+
+        // Download the survey questions and schedule the surveys
+        SurveyDownloader.downloadSurveys(applicationContext)
+
+        // Create new data files, these will now have a patientID prepended to those files
+        TextFileManager.initialize(applicationContext)
+        TextFileManager.makeNewFilesForEverything()
+
+        //This is important.  we need to start timers...
+        backgroundService.doSetup()
+
+        // Start the Main Screen Activity, destroy this activity
+        startActivity(Intent(applicationContext, LoadingActivity::class.java))
+        finish()
+    }
+}
---
+++ app/src/main/java/org/beiwe/app/ui/registration/PhoneNumberEntryActivity.kt
@@ -0,0 +1,64 @@
+package org.beiwe.app.ui.registration
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_phone_number_entry.*
+import org.beiwe.app.R
+import org.beiwe.app.RunningBackgroundServiceActivity
+import org.beiwe.app.storage.PersistentData
+import org.beiwe.app.survey.TextFieldKeyboard
+import org.beiwe.app.ui.utils.AlertsManager
+
+class PhoneNumberEntryActivity : RunningBackgroundServiceActivity() {
+    private val phoneNumberLength = 10
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_number_entry)
+
+        val textFieldKeyboard = TextFieldKeyboard(applicationContext)
+        if (PersistentData.getCallClinicianButtonEnabled()) {
+            textFieldKeyboard.makeKeyboardBehave(primaryCareNumber)
+        } else {
+            primaryCareNumberLabel.visibility = View.GONE
+            primaryCareNumber.visibility = View.GONE
+        }
+        if (PersistentData.getCallResearchAssistantButtonEnabled()) {
+            textFieldKeyboard.makeKeyboardBehave(passwordResetNumber)
+        } else {
+            passwordResetNumberLabel.visibility = View.GONE
+            passwordResetNumber.visibility = View.GONE
+        }
+    }
+
+    fun checkAndPromptConsent(view: View?) {
+        val primary = primaryCareNumber.text.toString().replace("\\D+".toRegex(), "")
+        val reset = passwordResetNumber.text.toString().replace("\\D+".toRegex(), "")
+        if (PersistentData.getCallClinicianButtonEnabled()) {
+            if (primary == null || primary.length == 0) {
+                AlertsManager.showAlert(getString(R.string.enter_clinician_number), this)
+                return
+            }
+            if (primary.length != phoneNumberLength) {
+                AlertsManager.showAlert(String.format(getString(R.string.phone_number_length_error), phoneNumberLength), this)
+                return
+            }
+        }
+        if (PersistentData.getCallResearchAssistantButtonEnabled()) {
+            if (reset == null || reset.length == 0) {
+                AlertsManager.showAlert(getString(R.string.enter_research_assitant_number), this)
+                return
+            }
+            if (reset.length != phoneNumberLength) {
+                AlertsManager.showAlert(String.format(getString(R.string.phone_number_length_error), phoneNumberLength), this)
+                return
+            }
+        }
+        PersistentData.setPrimaryCareNumber(primary)
+        PersistentData.setPasswordResetNumber(reset)
+        startActivity(Intent(applicationContext, ConsentFormActivity::class.java))
+        finish()
+    }
+}
