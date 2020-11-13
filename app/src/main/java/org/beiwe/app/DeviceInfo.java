75b8f38dd4260d3f025735823fbd1d08bd6b9de9
Josh Zagorsky <josh@zagaran.com>
2020-11-12 23:15:19 -0500

    Updates app version to 3.0.0, level 44

--- app/src/main/java/org/beiwe/app/DeviceInfo.java
+++ app/src/main/java/org/beiwe/app/DeviceInfo.java
@@ -78,7 +78,9 @@ public class DeviceInfo {
 	 * 42: app version 2.6.2, Deletes app data/registration for APK-installed (non-Google Play
 	 *      Store-installed) app
 	 * 43: app version 2.6.3, Fixes permissions request infinite loop in the onnelaLabServer build
-	 *      variant  */
+	 *      variant
+	 * 44: app version 3.0.0, Can receive survey push notifications. Upgrades compile SDK target to
+	 *      Android 10. Blocks registration if doesn't download key and app settings. */
 
 	private static String androidID;
 	private static String bluetoothMAC;
