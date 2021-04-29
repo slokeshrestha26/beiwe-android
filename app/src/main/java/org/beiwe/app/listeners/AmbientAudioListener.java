d18971ae37dac13d81d53de58c588eeae8bd581e
Josh Zagorsky <josh@zagaran.com>
2021-04-29 17:56:38 -0400

    Encrypts ambient audio file after recording

--- app/src/main/java/org/beiwe/app/listeners/AmbientAudioListener.java
+++ app/src/main/java/org/beiwe/app/listeners/AmbientAudioListener.java
@@ -2,7 +2,10 @@ package org.beiwe.app.listeners;
 
 import android.content.Context;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.util.Log;
+
+import org.beiwe.app.storage.AudioFileManager;
 
 import java.io.IOException;
 
@@ -11,8 +14,9 @@ import java.io.IOException;
  */
 public class AmbientAudioListener {
     private static AmbientAudioListener ambientAudioListenerInstance = null;
-    protected static MediaRecorder mRecorder;
     private static Context appContext;
+    private static MediaRecorder mRecorder;
+    private static String filenameExtension = ".mp4";
     public static final String unencryptedAudioFilename = "tempUnencryptedAmbientAudioFile";
 
     private AmbientAudioListener() {};
@@ -21,12 +25,13 @@ public class AmbientAudioListener {
         return appContext.getFilesDir().getAbsolutePath() + "/" + unencryptedAudioFilename;
     }
 
-    public static synchronized AmbientAudioListener startRecording(Context applicationContext) {
+    public static synchronized void startRecording(Context applicationContext) {
+        // Only instantiate the AmbientAudioRecorder if it has not yet been instantiated!
         if (ambientAudioListenerInstance == null) {
-            Log.i("ambient", "AmbientAudioListener is being instantiated");
-            appContext = applicationContext;
+            Log.e("ambient", "startRecording!");
             // Instantiate the AmbientAudioListener itself
             ambientAudioListenerInstance = new AmbientAudioListener();
+            appContext = applicationContext;
             // Start the Media Recorder
             mRecorder = new MediaRecorder();
             mRecorder.reset();
@@ -44,10 +49,46 @@ public class AmbientAudioListener {
                 // TODO: print a line to the app log file
             }
             mRecorder.start();
-        } else {
-            Log.i("ambient", "AmbientAudioListener already existed");
         }
-        Log.i("ambient", "starting recording now!");
-        return ambientAudioListenerInstance;  // TODO: maybe we don't need to return anything
+    }
+
+
+    public static synchronized void encryptAmbientAudioFile() {
+        Log.e("ambient", "called encryptAmbientAudioFile");
+        // First, check if the audio recorder exists
+        if (ambientAudioListenerInstance != null) {
+            Log.e("ambient", "preparing to halt ambient audio collection");
+            // TODO: check if mRecorder is currently recording
+            mRecorder.stop();
+            mRecorder.reset();
+            mRecorder.release();
+            mRecorder = null; // TODO: is this necessary? Or can we just resume it later?
+            new EncryptAmbientAudioFileTask().execute();
+        }
+    }
+
+
+    // TODO: is there a problem with EncryptAmbientAudioFileTask being static?
+    private static class EncryptAmbientAudioFileTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            Log.e("ambient", "encrypt audio file, onPreExecute()");
+            // TODO: set the filename, and blacklist it from being uploaded
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.e("ambient", "encrypt audio file, doing in background...");
+            AudioFileManager.encryptAudioFile(getUnencryptedAudioFilepath(), filenameExtension, null, appContext);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void arg) {
+            Log.e("ambient", "encrypt audio file, onPostExecute()");
+            // TODO: delete the unencrypted audio file
+            // TODO: remove the filename from the blacklist, and allow it to be uploaded
+            // TODO: resume recording, to a new unencrypted audio file
+        }
     }
 }
