fae4063dd41ef1f5eb740ca029dcfaf7018ef64f
Eli Jones <biblicabeebli@gmail.com>
2022-10-03 19:59:39 -0400

    [cleanup] tiny change

--- app/src/main/java/org/beiwe/app/utils.kt
+++ app/src/main/java/org/beiwe/app/utils.kt
@@ -5,39 +5,39 @@ import android.util.Log
 val APP_NAME = "Beiwe"
 
 // I want a gorram print command.
-fun printi(text: Any) {
+fun printi(text: Any?) {
     Log.i(APP_NAME, "" + text)
 }
-fun printi(tag: String, text: Any) {
+fun printi(tag: String, text: Any?) {
     Log.i(tag, "" + text)
 }
-fun printe(text: Any) {
+fun printe(text: Any?) {
     Log.e(APP_NAME, "" + text)
 }
-fun printe(tag: String, text: Any) {
+fun printe(tag: String, text: Any?) {
     Log.e(tag, "" + text)
 }
-fun printv(text: Any) {
+fun printv(text: Any?) {
     Log.v(APP_NAME, "" + text)
 }
-fun printv(tag: String, text: Any) {
+fun printv(tag: String, text: Any?) {
     Log.v(tag, "" + text)
 }
-fun printw(text: Any) {
+fun printw(text: Any?) {
     Log.w(APP_NAME, "" + text)
 }
-fun printw(tag: String, text: Any) {
+fun printw(tag: String, text: Any?) {
     Log.w(tag, "" + text)
 }
-fun printd(text: Any) {
+fun printd(text: Any?) {
     Log.d(APP_NAME, "" + text)
 }
-fun printd(tag: String, text: Any) {
+fun printd(tag: String, text: Any?) {
     Log.d(tag, "" + text)
 }
-fun print(text: Any) {
+fun print(text: Any?) {
     printd(text)
 }
-fun print(tag: String, text: Any) {
+fun print(tag: String, text: Any?) {
     printd(tag, text)
 }
