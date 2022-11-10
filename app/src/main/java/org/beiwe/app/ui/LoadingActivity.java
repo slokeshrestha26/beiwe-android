6ca726b6df4fb37cef053e26c8af3b699e1b7034
Eli Jones <biblicabeebli@gmail.com>
2022-11-10 17:55:32 -0500

    [accelerometer-upgrades] cleanup and clarification in loadingactivity and runningbackgroundserviceactivity

--- app/src/main/java/org/beiwe/app/ui/LoadingActivity.java
+++ app/src/main/java/org/beiwe/app/ui/LoadingActivity.java
@@ -31,74 +31,65 @@ import io.sentry.dsn.InvalidDsnException;
  * check out those classes if you are confused) that pops up when the user opens the app.
  * This activity runs some simple checks to make sure that the device can actually run the app,
  * and then bumps the user to the correct screen (Register or MainMenu).
- * 
+ *
  * note: this cannot be a SessionActvity, doing so would cause it to instantiate itself infinitely when a user is logged out. 
  * @author Eli Jones, Dor Samet */
 
 public class LoadingActivity extends RunningBackgroundServiceActivity {
 	
-	//TODO: Low priority.  Eli.  Why does this reimplement functionality in RunningBackgroundService? investigate.
-	protected MainService mainService;
-	protected boolean isBound = false;
-	
-	/**The ServiceConnection Class is our trigger for events that rely on the BackgroundService */
-	protected ServiceConnection backgroundServiceConnection = new ServiceConnection() {
-	    @Override
-	    public void onServiceConnected(ComponentName name, IBinder binder) {
-	        // Log.d("loading ServiceConnection", "Main Service Connected");
-	        BackgroundServiceBinder some_binder = (BackgroundServiceBinder) binder;
-	        mainService = some_binder.getService();
-	        isBound = true;
-	        loadingSequence();
-	    }
-
-	    @Override
-	    public void onServiceDisconnected(ComponentName name) {
-	        // Log.d("loading ServiceConnection", "Main Service Disconnected");
-	        mainService = null;
-	        isBound = false;
-	    }
+	/** for some reason we have to override the serviceconnection in order to call finish()
+	 * (inside loadingSequence()). otherwise we can't unbind the background service.
+	 * IllegalArgumentException: Service not registered: org.beiwe.app.RunningBackgroundServiceActivit...
+	 * using the mainServiceConnection variable is the least messy way that works. */
+	protected ServiceConnection mainServiceConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected (ComponentName name, IBinder binder) {
+			// Log.d("loading ServiceConnection", "Main Service Connected");
+			BackgroundServiceBinder some_binder = (BackgroundServiceBinder) binder;
+			mainService = some_binder.getService();
+			loadingSequence();
+		}
+		
+		@Override
+		public void onServiceDisconnected (ComponentName name) {
+			// Log.d("loading ServiceConnection", "Main Service Disconnected");
+			mainService = null;
+		}
 	};
-	
 	
 	/**onCreate - right now it just calls on checkLogin() in SessionManager, and moves the activity
 	 * to the appropriate page. In the future it could hold a splash screen before redirecting activity. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Context ctx = this.getApplicationContext();
-
+		
 		try {
 			String sentryDsn = BuildConfig.SENTRY_DSN;
-			Sentry.init(sentryDsn, new AndroidSentryClientFactory(ctx));
+			Sentry.init(sentryDsn, new AndroidSentryClientFactory(getApplicationContext()));
+		} catch (InvalidDsnException ie) {
+			Sentry.init(new AndroidSentryClientFactory(getApplicationContext()));
 		}
-		catch (InvalidDsnException  ie){
-			Sentry.init(new AndroidSentryClientFactory(ctx));
-		}
-
+		
 		setContentView(R.layout.activity_loading);
-				
-		if ( testHashing() ) {
+		
+		if (testHashing()) {
 			Intent startingIntent = new Intent(this.getApplicationContext(), MainService.class);
 			startingIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
 			// ContextCompat correctly handles old and new android APIs
-			ContextCompat.startForegroundService(ctx, startingIntent);
-			bindService( startingIntent, backgroundServiceConnection, Context.BIND_AUTO_CREATE);
+			ContextCompat.startForegroundService(getApplicationContext(), startingIntent);
+			bindService(startingIntent, mainServiceConnection, Context.BIND_AUTO_CREATE);
+		} else {
+			failureExit();
 		}
-		else { failureExit(); }
-		
-		/* In order to have additional compatibility tests we need to guarantee that the main service is already running,
-		this is complex, and all compatibility documentation for android indicates that the encryption we use are implemented for all 
-		versions of android.  is currently a very low priority problem.  The following lines of code tends to crash the app.
-		LoginManager.initialize( getApplicationContext() ); // probably fixed when we moved the LoginManager initialization to an earlier point in the activity cycle.
-		if ( !testEncryption( ) ) { failureExit(); } //probably still broken. */
 	}
 	
-
+	
 	/**CHecks whether device is registered, sends user to the correct screen. */
-	private void loadingSequence() {		
+	private void loadingSequence () {
 		//if the device is not registered, push the user to the register activity
-		if ( !PersistentData.getIsRegistered() ){ startActivity(new Intent(this, RegisterActivity.class) ); }
+		if (!PersistentData.getIsRegistered()) {
+			startActivity(new Intent(this, RegisterActivity.class));
+		}
 		//if device is registered push user to the main menu.
 		else {
 			if (BuildConfig.APP_IS_BETA) {
@@ -107,7 +98,7 @@ public class LoadingActivity extends RunningBackgroundServiceActivity {
 				startActivity(new Intent(this, MainMenuActivity.class));
 			}
 		}
-		unbindService(backgroundServiceConnection);
+		unbindService(mainServiceConnection);
 		finish(); //destroy the loading screen
 	}
 	
@@ -118,31 +109,22 @@ public class LoadingActivity extends RunningBackgroundServiceActivity {
 	
 	/**Tests whether the device can run the hash algorithm the app requires
 	 * @return boolean of whether hashing works */
-	private Boolean testHashing() {
+	private Boolean testHashing () {
 		// Runs the unsafe hashing function and catches errors, if it catches errors.
 		// The hashMAC function does not need to be tested here because it should not actually blow up.
 		// The source indicates that it should not blow up.
-		try { EncryptionEngine.unsafeHash("input"); }
-		catch (NoSuchAlgorithmException noSuchAlgorithm) { return false; }
-		catch (UnsupportedEncodingException unSupportedEncoding) { return false; }
+		try {
+			EncryptionEngine.unsafeHash("input");
+		} catch (NoSuchAlgorithmException noSuchAlgorithm) {
+			return false;
+		} catch (UnsupportedEncodingException unSupportedEncoding) {
+			return false;
+		}
 		return true;
 	}
 	
-//	/** Returns true if the device cannot use the necessary encryption.
-//	 * I apologize for the double negative, but the boolean logic is stupid if .*/
-//	private boolean testEncryption() {
-//		if ( LoginManager.isRegistered() ) {	
-//			byte[] testKey = EncryptionEngine.newAESKey();
-//			try { EncryptionEngine.encryptAES("test", testKey); }
-//			catch (InvalidKeyException e) { return false; } //device does not support aes
-//			catch (InvalidKeySpecException e) { return false; } //device does not support rsa
-//		}
-//		return true;
-//	}
-	
-
 	/**Displays error, then exit.*/
-	private void failureExit() {
-		AlertsManager.showErrorAlert( getString( R.string.invalid_device), this, 1);
+	private void failureExit () {
+		AlertsManager.showErrorAlert(getString(R.string.invalid_device), this, 1);
 	}
 }
